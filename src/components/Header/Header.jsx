dab4bdcf955b65bb906e7ad3fc2a0687652d0436
vutrongthang <100742325+vutrongthang@users.noreply.github.com>
2023-04-23 19:24:24 +0700

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import UserNav from "./UserNav";
 import { useSelector } from "react-redux";
@@ -11,6 +11,11 @@ export default function Header() {
     (total, item) => total + item.quantity,
     0
   );
+  const [showCategory, setShowCategory] = useState(false);
+
+  const handleCategoryClick = () => {
+    setShowCategory(!showCategory);
+  };
   return (
     <div className="shadow-lg sticky top-0 z-30 w-full px-2 py-2 bg-white sm:px-4 ">
       <div className="container mx-auto flex justify-between items-center h-20">
@@ -18,8 +23,43 @@ export default function Header() {
           Thương mại điện tử
         </span>
         <div className="space-x-5 font-medium">
-          <span>Loại Hàng</span>
-          <span>Cửa Hàng</span>
+          <span>
+            <Link style={{ textDecoration: "none" }} to="/">
+              Cửa Hàng
+            </Link>
+          </span>
+          <span className="cursor-pointer" onClick={handleCategoryClick}>
+            Loại Hàng
+          </span>
+          {showCategory && (
+            <div className="mr-6 absolute bg-white border border-gray-300 z-10 mt-2 py-2 w-64">
+              <Link
+                to="/category/1"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Thực phẩm tươi sống
+              </Link>
+              <Link
+                to="/category/2"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Đồ uống
+              </Link>
+              <Link
+                to="/category/3"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Đồ gia dụng
+              </Link>
+              <Link
+                to="/category/4"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Quần áo
+              </Link>
+            </div>
+          )}
+
           <span>
             <Link to="/" style={{ textDecoration: "none" }}>
               Sản Phẩm
